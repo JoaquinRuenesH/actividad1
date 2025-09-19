0ef7365a4628371503a874c43c97cf9e0bc96268
JoaquinRuenesH <A01661196@tec.mx>
2025-09-19 12:40:26 -0600

    Agregación de color morado

--- paint.py
+++ paint.py
@@ -38,8 +38,19 @@ def square(start, end):
 
 def circle(start, end):
     """Draw circle from start to end."""
-    pass  # TODO
+    # TODO
+    '''
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
 
+    for count in range(4):
+        forward(end.x - start.x)
+        left(90)
+
+    end_fill()
+'''
 
 def rectangle(start, end):
     """Draw rectangle from start to end."""
@@ -79,6 +90,7 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+onkey(lambda: color('purple'),'M')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
